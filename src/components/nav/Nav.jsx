7c69b47a2a6cfc1e4d57fbff59e5a711220513cd
franklin-raph <igboekwulusifranklin@gmail.com>
2024-06-13 23:59:22 +0100

    update

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,27 +6,29 @@ const Nav = () => {
   return (
     <div>
       <TopInfo />
-      <nav className='flex items-center justify-between bg-[#FBFBFB] py-[16px] px-[3rem]'>
-        <Link to='/' className='font-bold text-[20px]'>Futurevote</Link>
-        <ul className='flex items-center justify-between text-[#00000099] gap-[1rem] text-[14px]'>
-          <li>
-            <Link to='/'>Individual</Link>
-          </li>
-          <li>
-            <Link to='/'>Political parties</Link>
-          </li>
-          <li>
-            <Link to='/'>Elections</Link>
-          </li>
-          <li>
-            <Link to='/'>News</Link>
-          </li>
-          <li>
-            <Link to='/'>Resources</Link>
-          </li>
-        </ul>
-        <button className='bg-black py-2 px-[1.2rem] text-white rounded-[7px] text-[14px]'>Connect Wallet</button>
-      </nav>
+      <div className='bg-[#FBFBFB]'>
+        <nav className='flex items-center w-[1200px] mx-auto justify-between py-[16px]'>
+          <Link to='/' className='font-bold text-[20px]'>Futurevote</Link>
+          <ul className='flex items-center justify-between text-[#00000099] gap-[1rem] text-[14px]'>
+            <li>
+              <Link to='/'>Individual</Link>
+            </li>
+            <li>
+              <Link to='/'>Political parties</Link>
+            </li>
+            <li>
+              <Link to='/'>Elections</Link>
+            </li>
+            <li>
+              <Link to='/'>News</Link>
+            </li>
+            <li>
+              <Link to='/'>Resources</Link>
+            </li>
+          </ul>
+          <button className='bg-black py-2 px-[1.2rem] text-white rounded-[7px] text-[14px]'>Connect Wallet</button>
+        </nav>
+      </div>
     </div>
   )
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaLinkedin, FaXTwitter, FaYoutube } from "react-icons/fa6";
+import { AiFillInstagram } from "react-icons/ai";
+
 
 const Home = () => {
   return (
@@ -31,7 +35,7 @@ const Home = () => {
         </div>
       </div>
       <div>
-        <img src="./images/meso.svg" alt="" />
+        <img src="./images/meso.svg" className='mx-auto w-full' alt="" />
         <div className='flex items-center text-primary-color justify-between mt-[2rem] w-[1200px] mx-auto'>
           <div className='text-center'>
             <p className='font-[500] text-[45px]'>99%</p>
@@ -57,28 +61,77 @@ const Home = () => {
         <div className='flex items-start justify-between'>
           <div className='w-[48%] h-[500px] bg-[#C4C4C4]'></div>
           <div className='w-[48%]'>
-            <p className='text-[200px] text-[#3939391C]'>01</p>
-            <p className='text-[22px] font-[500]'>Unmatched Security</p>
+            <div className='flex items-center'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="179" height="241" viewBox="0 0 179 241" fill="none">
+                <path d="M89.4975 241.003C26.1463 241.003 0.336548 186.366 0.336548 120.669C0.336548 54.9714 26.1463 0 89.4975 0C152.849 0 178.658 54.9714 178.658 120.669C178.658 186.366 152.849 241.003 89.4975 241.003ZM89.4975 214.187C133.743 214.187 148.491 169.942 148.491 120.669C148.491 71.3958 133.743 26.8153 89.4975 26.8153C45.2522 26.8153 30.5038 71.3958 30.5038 120.669C30.5038 169.942 45.2522 214.187 89.4975 214.187Z" fill="#393939" fill-opacity="0.11"/>
+              </svg>
+              <img src="./images/1.svg" alt="" />
+            </div>
+            {/* <p className='text-[200px] text-[#3939391C]'>01</p> */}
+            <p className='text-[22px] font-[500] mb-[1.2rem] mt-[1rem]'>Unmatched Security</p>
             <p>Our platform leverages advanced blockchain technology to ensure that every vote is securely encrypted and tamper-proof. With cutting-edge cryptographic protocols, your vote remains confidential and protected from unauthorized access.</p>
           </div>
         </div>
-        <div className='flex items-start justify-between flex-row-reverse my-10'>
+        <div className='flex items-start justify-between flex-row-reverse my-20'>
           <div className='w-[48%] h-[500px] bg-[#C4C4C4]'></div>
           <div className='w-[48%]'>
-            <p className='text-[200px] text-[#3939391C]'>02</p>
-            <p className='text-[22px] font-[500]'>Smart Conract</p>
+            <div className='flex items-center mt-[60px]'>
+               <svg xmlns="http://www.w3.org/2000/svg" width="147" height="191" viewBox="0 0 147 191" fill="none">
+                <path d="M73.4898 190.598C21.9412 190.598 0 147.244 0 95.431C0 43.618 21.9412 0 73.4898 0C125.038 0 146.98 43.618 146.98 95.431C146.98 147.244 125.038 190.598 73.4898 190.598ZM73.4898 164.427C105.741 164.427 116.315 133.498 116.315 95.431C116.315 57.3643 105.741 26.1708 73.4898 26.1708C41.2389 26.1708 30.4004 57.3643 30.4004 95.431C30.4004 133.498 41.2389 164.427 73.4898 164.427Z" fill="#DDDEDF"/>
+              </svg>
+              <img src="./images/2.svg" alt="" />
+            </div>
+            <p className='text-[22px] font-[500] mt-[1rem] mb-[1.2rem]'>Smart Conract</p>
             <p>Utilize the power of smart contracts to automate and enforce electoral rules and procedures. These self-executing contracts ensure that all voting processes are fair, efficient, and free from human error or manipulation.</p>
           </div>
         </div>
         <div className='flex items-start justify-between'>
           <div className='w-[48%] h-[500px] bg-[#C4C4C4]'></div>
-          <div className='w-[48%]'>
-            <p className='text-[200px] text-[#3939391C]'>03</p>
-            <p className='text-[22px] font-[500]'>Voter Anonymity</p>
+          <div className='w-[48%] mt-[60px]'>
+            <div className='flex items-center'>
+              <img src="./images/0.svg" alt="" />
+              <img src="./images/3.svg" alt="" />
+            </div>
+            <p className='text-[22px] font-[500] mb-[1.2rem] mt-[1rem]'>Voter Anonymity</p>
             <p>Preserving voter privacy is paramount. Our platform ensures that while every vote is recorded and verifiable, the identity of each voter remains anonymous, protecting individuals from coercion and ensuring free expression of choice.</p>
           </div>
         </div>
       </div>
+      <footer className='bg-[#464646] py-[5rem] mt-[14rem]'>
+        <div>
+          <ul className='flex items-center justify-center text-white gap-[2rem] text-[14px]'>
+            <li>
+              <Link to='/'>Individual</Link>
+            </li>
+            <li>
+              <Link to='/'>Political parties</Link>
+            </li>
+            <li>
+              <Link to='/'>Elections</Link>
+            </li>
+            <li>
+              <Link to='/'>News</Link>
+            </li>
+            <li>
+              <Link to='/'>Resources</Link>
+            </li>
+          </ul>
+          <ul className='flex items-center justify-center text-white gap-[1rem] text-[16px] mt-[2rem]'>
+            <li>
+              <Link to='#'><FaXTwitter /></Link>
+            </li>
+            <li>
+              <Link to='#'><AiFillInstagram /></Link>
+            </li>
+            <li>
+              <Link to='#'><FaLinkedin /></Link>
+            </li>
+            <li>
+              <Link to='#'><FaYoutube /></Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </div>
   )
 }

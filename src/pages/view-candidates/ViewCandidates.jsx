d6ed2682a5c33abab97ad02c3c7d9746067dcfda
franklin-raph <igboekwulusifranklin@gmail.com>
2024-06-14 13:24:31 +0100

    update

--- src/pages/view-candidates/ViewCandidates.jsx
+++ src/pages/view-candidates/ViewCandidates.jsx
@@ -11,8 +11,8 @@ const ViewCandidates = () => {
     <div className='w-[1200px] mx-auto pb-[5rem]'>
         <p className='text-[24px] font-[600] mt-7'>Welcome Adeyemi,</p>
         <p className='text-[#464646] mt-2'>Explore candidate profiles, manifestos, and election information.</p>
-        <div className='flex items-center justify-between'>
-            <div className='flex items-center gap-2 text-[#414141] mt-[3rem]'>
+        <div className='flex items-center justify-between mt-[3.5rem]'>
+            <div className='flex items-center gap-2 text-[#414141]'>
                 <TbSmartHome className='text-[1.2rem]'/>
                 <RxChevronRight />
                 <p>Election</p>
@@ -37,39 +37,157 @@ const ViewCandidates = () => {
                 </div>
             </div>
         </div>
-        <div className='bg-white p-1 rounded-[16px] w-[600px] mx-auto shadow-lg'>
-            <div className='bg-[#FCFBFC] rounded-[16px]'>
-                <img src="./images/toptexture.svg" className='mx-auto' alt="" />
-                <div className='mt-[-70px]'>
-                <p className='font-[600] text-[24px] text-center'>Please Fill in KYC Details</p>
-                <p className='text-[#333] text-center'>These details would help us set up your account profile</p>
+        <div className='grid grid-cols-2 gap-[2rem] mt-[3rem]'>
+            <div className='bg-white p-1 rounded-[16px] w-[600px] mx-auto shadow-lg'>
+                <div className='bg-[#FCFBFC] rounded-[16px]'>
+                    <img src="./images/toptexture.svg" className='mx-auto' alt="" />
+                    <div className='mt-[-70px]'>
+                        <p className='font-[600] text-[24px] text-center'>Sarah Gregman Obinwanne</p>
+                        <p className='text-[#333333] text-[14px] tracking-[-0.42px] text-center'>These are the details on their account profile.</p>
+                    </div>
+                    <div className='mt-[4rem] px-10'>
+                        <div className='flex gap-[1rem]'>
+                            <img src="./images/image-2.svg" className='self-start' alt="" />
+                            <div className="">
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Name:</span>
+                                    <span className="w-3/4">Sarah Gregman Obinwanne</span>
+                                </div>
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Age:</span>
+                                    <span className="w-3/4">62 Years</span>
+                                </div>
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Bio:</span>
+                                    <span className="w-3/4">
+                                        Sarah Gregman is Nigerian politician, statesman, political activist and business executive. He served twice as the Governor of Anambra State from 2006 to 2010, and a second term until 2014.
+                                    </span>
+                                </div>
+                                <div className="flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Party:</span>
+                                    <span className="w-3/4">Sushi Party</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mb-6 mt-16'>
+                            <button className='bg-primary-color mx-auto block text-white w-full py-2 rounded-[8px] mt-3'>Vote</button>
+                            <button className='w-full mx-auto block text-primary-color font-[600] py-2 rounded-[8px] mt-3'>View Manifesto</button>
+                        </div>
+                    </div>
                 </div>
-                <div className='w-[80%] mx-auto mt-[4rem] grid gap-[1.5rem]'>
-                <div className='w-full'>
-                    <label htmlFor="fullname" className='block text-[#344054] mb-[6px] text-[14px]'>Full Name</label>
-                    <input type="text" className='border border-[#D0D5DD] rounded-[8px] w-full py-[6px] px-2' />
+            </div>
+            <div className='bg-white p-1 rounded-[16px] w-[600px] mx-auto shadow-lg'>
+                <div className='bg-[#FCFBFC] rounded-[16px]'>
+                    <img src="./images/toptexture.svg" className='mx-auto' alt="" />
+                    <div className='mt-[-70px]'>
+                        <p className='font-[600] text-[24px] text-center'>Smith Rowe Jnr.</p>
+                        <p className='text-[#333333] text-[14px] tracking-[-0.42px] text-center'>These are the details on their account profile.</p>
+                    </div>
+                    <div className='mt-[4rem] px-10'>
+                        <div className='flex gap-[1rem]'>
+                            <img src="./images/image-1.svg" className='self-start' alt="" />
+                            <div className="">
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Name:</span>
+                                    <span className="w-3/4">Smith Rowe Jnr.</span>
+                                </div>
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Age:</span>
+                                    <span className="w-3/4">46 Years</span>
+                                </div>
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Bio:</span>
+                                    <span className="w-3/4">
+                                        Chief Smith Rowe is a Nigerian politician who is the 16th and current president of Nigeria. He was the governor of Lagos State from 1999 to 2007, and senator for Lagos West in the Third Republic.
+                                    </span>
+                                </div>
+                                <div className="flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Party:</span>
+                                    <span className="w-3/4">Blue Party</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mb-6 mt-16'>
+                            <button className='bg-primary-color mx-auto block text-white w-full py-2 rounded-[8px] mt-3'>Vote</button>
+                            <button className='w-full mx-auto block text-primary-color font-[600] py-2 rounded-[8px] mt-3'>View Manifesto</button>
+                        </div>
+                    </div>
                 </div>
-                <div className='w-full'>
-                    <label htmlFor="fullname" className='block text-[#344054] mb-[6px] text-[14px]'>Date of Birth</label>
-                    <input type="date" className='border border-[#D0D5DD] rounded-[8px] w-full py-[6px] px-2' />
+            </div>
+            <div className='bg-white p-1 rounded-[16px] w-[600px] mx-auto shadow-lg'>
+                <div className='bg-[#FCFBFC] rounded-[16px]'>
+                    <img src="./images/toptexture.svg" className='mx-auto' alt="" />
+                    <div className='mt-[-70px]'>
+                        <p className='font-[600] text-[24px] text-center'>Smith Rowe Jnr.</p>
+                        <p className='text-[#333333] text-[14px] tracking-[-0.42px] text-center'>These are the details on their account profile.</p>
+                    </div>
+                    <div className='mt-[4rem] px-10'>
+                        <div className='flex gap-[1rem]'>
+                            <img src="./images/image-1.svg" className='self-start' alt="" />
+                            <div className="">
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Name:</span>
+                                    <span className="w-3/4">Smith Rowe Jnr.</span>
+                                </div>
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Age:</span>
+                                    <span className="w-3/4">46 Years</span>
+                                </div>
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Bio:</span>
+                                    <span className="w-3/4">
+                                        Chief Smith Rowe is a Nigerian politician who is the 16th and current president of Nigeria. He was the governor of Lagos State from 1999 to 2007, and senator for Lagos West in the Third Republic.
+                                    </span>
+                                </div>
+                                <div className="flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Party:</span>
+                                    <span className="w-3/4">Blue Party</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mb-6 mt-16'>
+                            <button className='bg-primary-color mx-auto block text-white w-full py-2 rounded-[8px] mt-3'>Vote</button>
+                            <button className='w-full mx-auto block text-primary-color font-[600] py-2 rounded-[8px] mt-3'>View Manifesto</button>
+                        </div>
+                    </div>
                 </div>
-                <div className='w-full'>
-                    <label htmlFor="fullname" className='block text-[#344054] mb-[6px] text-[14px]'>National Identity Number</label>
-                    <input type="text" className='border border-[#D0D5DD] rounded-[8px] w-full py-[6px] px-2' />
-                </div>
-                <div className='w-full'>
-                    <label htmlFor="fullname" className='block text-[#344054] mb-[6px] text-[14px]'>Phone Number</label>
-                    <input type="text" className='border border-[#D0D5DD] rounded-[8px] w-full py-[6px] px-2' />
-                </div>
-                <div className='w-full'>
-                    <label htmlFor="fullname" className='block text-[#344054] mb-[6px] text-[14px]'>Address</label>
-                    <input type="text" className='border border-[#D0D5DD] rounded-[8px] w-full py-[6px] px-2' />
-                </div>
-                <button className='bg-light-green w-[80%] mx-auto block text-white py-2 rounded-[8px] mt-3'>Submit Kyc</button>
-                <div className='text-[14px]'>
-                    <p className='text-center text-[#ACB0B5] font-[600] mb-3 cursor-pointer'>Skip</p>
-                    <p className='text-center text-[#747272] mb-5'>Didn’t receive code? <span className='text-light-green cursor-pointer'>Resend</span> </p>
-                </div>
+            </div>
+            <div className='bg-white p-1 rounded-[16px] w-[600px] mx-auto shadow-lg'>
+                <div className='bg-[#FCFBFC] rounded-[16px]'>
+                    <img src="./images/toptexture.svg" className='mx-auto' alt="" />
+                    <div className='mt-[-70px]'>
+                        <p className='font-[600] text-[24px] text-center'>Sarah Gregman Obinwanne</p>
+                        <p className='text-[#333333] text-[14px] tracking-[-0.42px] text-center'>These are the details on their account profile.</p>
+                    </div>
+                    <div className='mt-[4rem] px-10'>
+                        <div className='flex gap-[1rem]'>
+                            <img src="./images/image-2.svg" className='self-start' alt="" />
+                            <div className="">
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Name:</span>
+                                    <span className="w-3/4">Sarah Gregman Obinwanne</span>
+                                </div>
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Age:</span>
+                                    <span className="w-3/4">62 Years</span>
+                                </div>
+                                <div className="mb-2 flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Bio:</span>
+                                    <span className="w-3/4">
+                                        Sarah Gregman is Nigerian politician, statesman, political activist and business executive. He served twice as the Governor of Anambra State from 2006 to 2010, and a second term until 2014.
+                                    </span>
+                                </div>
+                                <div className="flex">
+                                    <span className="w-1/4 font-[500] text-[#9E9D9D]">Party:</span>
+                                    <span className="w-3/4">Sushi Party</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mb-6 mt-16'>
+                            <button className='bg-primary-color mx-auto block text-white w-full py-2 rounded-[8px] mt-3'>Vote</button>
+                            <button className='w-full mx-auto block text-primary-color font-[600] py-2 rounded-[8px] mt-3'>View Manifesto</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
